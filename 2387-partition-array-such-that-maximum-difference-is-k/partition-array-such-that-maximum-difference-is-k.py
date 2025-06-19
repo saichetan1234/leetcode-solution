22e3195f1c123c41663158d008c2ebc22048c4a7
saichetan1234 <avsaichetan@gmail.com>
2025-06-19 23:43:52 +0530

    Time: 113 ms (21.65%) | Memory: 29.1 MB (32.78%) - LeetSync

--- 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/partition-array-such-that-maximum-difference-is-k.py
@@ -2,11 +2,9 @@ class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
         
         nums.sort()
-        count = 1
-        ans = nums[0]
+        arr=[]
 
-        for i in range(1,len(nums)):
-            if nums[i] - ans > k:
-                count = count +1
-                ans = nums[i]
-        return count
+        for i in range(len(nums)):
+            if len(arr)==0 or nums[i]-arr[-1]>k:
+                arr.append(nums[i])
+        return len(arr)
