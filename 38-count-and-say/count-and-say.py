666b22f692ebc4db4cbc53ff2dcacc8dc226c0ce
saichetan1234 <avsaichetan@gmail.com>
2025-06-21 23:19:53 +0530

    Time: 7 ms (75.42%) | Memory: 18 MB (20.71%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,18 +1,16 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        ans="1"
-        for i in range(n-1):
-            temp=""
-            count=1
-            for j in range(1,len(ans)+1):
-                if j==len(ans) or ans[j-1]!=ans[j]:
-                    temp+=str(count)+str(ans[j-1])
-                    count=1
-                else:
-                    count+=1
-            ans=temp
-        return ans
         
-
-
+        b= "1"
+        for i in range(n-1):
+            temp = ""
+            count = 1
+            for j in range(1,len(b)+1):
+                if(j==len(b) or b[j-1]!=b[j]):
+                    temp += str(count) + str(b[j-1])
+                    count= 1
+                else:
+                    count = count + 1
+            b = temp
+        return b 
         
