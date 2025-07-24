0ede68025c2c07c29ee0c5a72aa9de04699f9c34
saichetan1234 <avsaichetan@gmail.com>
2025-07-24 23:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (22.86%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,7 +2,7 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if digits=="":
             return []
-        phone={2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
+        phone={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
         ans=[]
         n=len(digits)
         def backtrack(index,path):
@@ -10,7 +10,7 @@ class Solution:
                 if "".join(path) not in ans:
                     ans.append("".join(path))
                 return 
-            for i in phone[int(digits[index])]:
+            for i in phone[digits[index]]:
                 path.append(i)
                 backtrack(index+1,path)
                 path.pop()
