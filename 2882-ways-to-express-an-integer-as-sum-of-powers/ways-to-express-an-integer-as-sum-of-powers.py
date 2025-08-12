eb579943dfc67be6731b63ae6d1f5e0656876203
saichetan1234 <avsaichetan@gmail.com>
2025-08-12 14:10:01 +0530

    Time: 2476 ms (9.01%) | Memory: 31.7 MB (34.59%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.py
@@ -6,21 +6,20 @@ class Solution:
             arr.append(j**x)
             j+=1
         m=len(arr)
-        base=n+1
-        dp={}
+        
+        dp=[[-1 for _ in range(n+1)] for _ in range(m)]
         def check(index,target):
             if target==0:
                 return 1
             if index>=m or target<0:
                 return 0
-            key=index*base+target
-            if key in dp:
-                return dp[key]
+            if dp[index][target]!=-1:
+                return dp[index][target]
             paths=0
             paths+=check(index+1,target)
             paths+=check(index+1,target-arr[index])
-            dp[key]=paths
-            return dp[key]
+            dp[index][target]=paths
+            return dp[index][target]
         return check(0,n) % (10**9+7)
 
         
