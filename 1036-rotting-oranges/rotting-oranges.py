bd9991652b14fb9934adb91165092cad1428d299
saichetan1234 <avsaichetan@gmail.com>
2025-08-28 17:46:01 +0530

    Time: 3 ms (81.47%) | Memory: 17.8 MB (71.59%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,40 +1,39 @@
 from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+       
         q = deque()
+        m = len(grid)
+        n = len(grid[0])
         fresh = 0
-        row  = len(grid)
-        col = len(grid[0])
-
-        for r in range((row)):
-            for c in range((col)):
-                if grid[r][c] == 2:
-                    q.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh = fresh + 1
         minutes = 0
-        direct = [(0,1),(1,0),(0,-1),(-1,0)]
 
-        while q and fresh >0:
-            for i in range(len(q)):
-                r,c = q.popleft()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    q.append([i,j])
+                elif grid[i][j] == 1:
+                    fresh = fresh + 1
+        direct = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        while q and fresh > 0:
+            for k in range(len(q)):
+                i,j = q.popleft()
+
                 for dr,dc in direct:
-                    nr = r + dr
-                    nc = c + dc 
+                    nr,nc = i + dr,j + dc
 
-                    if 0<=nr<row and 0<=nc<col and grid[nr][nc] ==1:
-                        grid[nr][nc] =2
-                        fresh = fresh -1
-                        q.append((nr,nc))
-            minutes = minutes + 1
+                    if 0<=nr<m and 0<=nc<n and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh  = fresh -1 
+                        q.append([nr,nc])
+            minutes = minutes +1
         
-        if fresh ==0:
+        if fresh == 0:
             return minutes
         else:
             return -1
 
 
 
-
-        
-        
+            
