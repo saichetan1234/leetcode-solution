fd1c07a02a1f7cae54cfec335164998f83070742
saichetan1234 <avsaichetan@gmail.com>
2026-01-12 16:06:56 +0530

    Time: 3 ms (50.86%) | Memory: 20.7 MB (5.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,17 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if(nums[i] + nums[j] == target):
-                    return [i,j]
+        
+        seen = {}
+
+        for i , num in enumerate(nums):
+            if target-num in seen:
+                return [seen[target-num],i]
+            seen[num] = i
 
 
 
        
-        
+      
+
