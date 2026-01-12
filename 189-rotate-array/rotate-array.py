0b41e4c5a35e8fff6b2564e699cf406b5a7a67d8
saichetan1234 <avsaichetan@gmail.com>
2026-01-12 19:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 26.7 MB (5.40%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,28 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
 
+        
         """
-        b = []
-        c = []
         k = k % len(nums)
-
-        for i in range(len(nums) - k,len(nums)):
-            b.append(nums[i])
-        print(b)
-
-        for i in range(0,len(nums) - k):
-            c.append(nums[i])
-        print(c)
-
-        d =  b + c
-        print(d)
-
-        for i in range(len(nums)):
-            nums[i] = d[i]
-        
-        print(nums[i])
+        nums[:] = nums[-k:] + nums[:-k]
 
 
+
+
+      
 
 
         
+       
+
