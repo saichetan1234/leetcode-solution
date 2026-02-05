f7a80c5334cc8b078a62e8e2796c9079384cca8a
saichetan1234 <avsaichetan@gmail.com>
2026-02-05 10:18:59 +0530

    Time: 5 ms (85.59%) | Memory: 21.1 MB (43.36%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -0,0 +1,19 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        left = 0
+        right = len(height)-1
+        left_max = height[left]
+        right_max = height[right]
+        ans = 0
+
+        while left<right:
+            if left_max<right_max:
+                left = left + 1
+                left_max = max(left_max,height[left])
+                ans = ans + left_max - height[left]
+            else:
+                right = right -1
+                right_max = max(right_max,height[right])
+                ans = ans + right_max - height[right]
+        return ans
