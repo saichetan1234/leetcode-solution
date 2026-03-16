a05469cb9995db12037cccba569129ebbaac027c
saichetan1234 <avsaichetan@gmail.com>
2026-03-16 14:58:16 +0530

    Time: 30 ms (81.40%) | Memory: 21.7 MB (98.82%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,18 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+
         sumi = 0
-        maps = {0:1}
-        count = 0
+        hashmap  = {0:1}
+        count  = 0
 
         for i in nums:
-            sumi = sumi + i
+            sumi  =  sumi + i 
 
-            if sumi - k in maps:
-                count  =  count + maps[sumi-k]
-
-            maps[sumi] = maps.get(sumi,0) + 1
+            if sumi - k in hashmap:
+                count  =  count + hashmap[sumi-k ]
+            
+            hashmap[sumi] = hashmap.get(sumi,0)+1
 
         return count
+        
+        
