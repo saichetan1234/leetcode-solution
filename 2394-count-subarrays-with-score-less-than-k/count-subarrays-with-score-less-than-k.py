0e5cefcf06fdb52c3e0a8959e77ea6fb24369a49
saichetan1234 <avsaichetan@gmail.com>
2026-03-27 22:20:31 +0530

    Time: 145 ms (51.67%) | Memory: 30.8 MB (65.34%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
@@ -6,6 +6,7 @@ class Solution:
 
         count  = 0
         l =0
+        b = []
 
         for i in range(len(nums)):
             sumi = sumi + nums[i]
@@ -14,7 +15,7 @@ class Solution:
                 sumi =  sumi - nums[l]
                 l = l + 1
 
-            count  =  count + (i-l+1)
+            count  =  count +(i-l+1)
 
         return count 
 
