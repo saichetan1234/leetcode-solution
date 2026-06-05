532b31a9406054a97608912b2ec0329657da50da
saichetan1234 <avsaichetan@gmail.com>
2026-06-05 14:50:33 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (28.60%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,14 +1,29 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
 
-        temp ={}
-        b=[]
+        l= 1
+        n = len(nums)
 
-        for i in nums:
-            temp[i] = temp.get(i,0) +1 
+        h = len(nums)-2
+        if n ==1:
+            return nums[n-1]
 
-        for key,value in temp.items():
-            if value == 1:
-                b.append(key)
-        return b[0]
+        if nums[1]!=nums[0]:
+            return nums[0]
+        if nums[n-1]!=nums[n-2]:
+            return nums[n-1]
+
+        while(l<=h):
+            mid  = (l+h)//2
+
+            if nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]:
+                return nums[mid]
+
+            if mid%2!=0 and nums[mid-1]==nums[mid] or mid %2 == 0 and nums[mid] == nums[mid +1]:
+                l = mid + 1
+            else:
+                h = mid -1
+        return -1
+
+       
         
